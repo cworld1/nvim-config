4849a3abcb3a3d58d045960c2b5de7e19e559442
CWorld <cworld0@qq.com>
2023-07-18 23:59:50 +0800

    feat(keymaps): :sparkles: add save and quit keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -17,12 +17,19 @@ keymap.set("n", "<leader>sv", "<C-w>v") -- 右侧新增窗口
 keymap.set("n", "<leader>sh", "<C-w>s") -- 底部新增窗口
 -- 开辟终端窗口
 keymap.set("n", "<leader>`", "<C-w>s :term<CR>")
+
+-- 文件操作
+-- 保存
+keymap.set("n", "<leader>w", ":w<CR>")
+-- 退出
+keymap.set("n", "<leader>q", ":q<CR>")
+
+-- 代码操作
 -- 删除行
 keymap.set("n", "xx", ":delete<CR>")
 -- 切换是否自动换行
 keymap.set('n', '<leader>x', ':set invwrap<CR>', { noremap = true })
-
--- 取消高亮
+-- 取消搜索高亮
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 -- ----- 插件 ----- --
