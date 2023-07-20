7ccedecf95cb87165b3c7d9a1493c031e6525437
CWorld <cworld0@qq.com>
2023-07-20 16:51:44 +0800

    feat(keymaps): :rocket: add more support about keymap and which-key

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,41 +1,57 @@
 -- 设置 leader 键
 vim.g.mapleader = " "
 
-local keymap = vim.keymap
-
--- ----- 插入模式 ----- --
-
-
--- ----- 视觉模式 ----- --
 -- 单行或多行移动
-keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<M-k>", ":m '>-2<CR>gv=gv")
 
--- ----- 正常模式 ----- --
--- 窗口
-keymap.set("n", "<leader>sv", "<C-w>v") -- 右侧新增窗口
-keymap.set("n", "<leader>sh", "<C-w>s") -- 底部新增窗口
--- 开辟终端窗口
-keymap.set("n", "<leader>`", "<C-w>s :term<CR>")
+local whichkeymaps = {
+  -- ----- Leader ----- --
+  ["<leader>"] = { -- 指定该快捷键组的前缀
+    name = "Leader", -- 指定该快捷键组的名称
 
--- 文件操作
--- 保存
-keymap.set("n", "<leader>w", ":w<CR>")
--- 退出
-keymap.set("n", "<leader>q", ":q<CR>")
+    -- 文件操作
+    w = { "<cmd>w<cr>", "Save" }, -- 保存
+    q = { "<cmd>q<cr>", "Quit" }, -- 退出
+    c = { "<cmd>bd<cr>", "Close buffer"},
 
--- 代码操作
--- 删除行
-keymap.set("n", "xx", ":delete<CR>")
--- 切换是否自动换行
-keymap.set('n', '<leader>x', ':set invwrap<CR>', { noremap = true })
--- 取消搜索高亮
-keymap.set("n", "<leader>nh", ":nohl<CR>")
+    -- 窗口
+    s = {
+      name = "Split window",
+      v = { "<C-w>v", "Split right" }, -- 右侧新增窗口
+      s = { "<C-w>s", "Split bottom" }, -- 底部新增窗口
+    },
+    ["`"] = { "<C-w>s <cmd>term<cr>", "Open terminal" }, -- 开辟终端窗口
 
--- ----- 插件 ----- --
--- NvimTreeToggle
-keymap.set("n", "<leader>b", ":NvimTreeToggle<CR>")
--- Bufferline
-keymap.set("n", "L", ":bnext<CR>")
-keymap.set("n", "H", ":bprevious<CR>")
+    -- 代码状态
+    x = { "<cmd>set invwrap<cr>", "Toggle wrap",
+    noremap = true }, -- 切换是否自动换行
+    n = { "<cmd>nohl<cr>", "Cancel search highlight" },
 
+    -- 插件
+    -- Telescope
+    f = {
+        name = "Telescope",
+        b = { "Find buffers" },
+        f = { "Find file" },
+        g = { "Find live grep" },
+        h = { "Find help" },
+        o = { "Find outlines" }
+    },
+    -- 返回主页 Dashboard
+    h = { "<cmd>NvimTreeClose<cr><cmd>Dashboard<cr>", "Dashboard" },
+    -- 侧栏切换 NvimTree
+    b = { "<cmd>NvimTreeToggle<cr>", "NvimTree" },
+  },
+
+  -- ----- Shift ----- --
+  -- 标签页切换 Bufferline
+  L = { "<cmd>BufferLineCycleNext<cr>", "Next buffer" },
+  H = { "<cmd>BufferLineCyclePrev<cr>", "Previous buffer" },
+
+  -- ----- Alt ----- --
+  ["<M-j>"] = { "<cmd>m .+1<cr>==", "Move line down" }, -- 下移一行
+  ["<M-k>"] = { "<cmd>m .-2<cr>==", "Move line up" }, -- 上移一行
+}
+
+return whichkeymaps
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,11 +1,5 @@
-require("which-key").register ({
-  f = {
-      name = "Telescope", -- 指定该快捷键组的名称
-      b = { "Find buffers" }, -- 创建新的快捷键绑定
-      f = { "Find file" }, -- 创建新的快捷键绑定
-      g = { "Find live grep" },
-      h = { "Find help" }
-  }
-}, {prefix = "<leader>"})
+local maps = require("core.keymaps")
+
+require("which-key").register(maps)
 
 require("which-key").setup()
