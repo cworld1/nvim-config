b8a0515fcd5e5eab832323bf0f34b42c5faffcbc
CWorld <cworld0@qq.com>
2023-07-26 18:00:42 +0800

    style(plugins): :art: update appearance of ui

--- lua/lsp/treesitter.lua
+++ lua/lsp/treesitter.lua
@@ -2,33 +2,29 @@ require("nvim-treesitter.configs").setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = {
     -- 静态文件
-    "vim", "vimdoc", "lua", -- Neovim 自身
+    "vim", "vimdoc", "lua",        -- Neovim 自身
     "markdown", "markdown_inline", -- Markdown
-    "json", "yaml", "toml", -- 数据存储
-    "gitignore", "dockerfile", -- 其他
+    "json", "yaml", "toml",        -- 数据存储
+    "gitignore", "dockerfile",     -- 其他
 
     -- 脚本文件
     "bash",
 
     -- 编程语言
     "c", "cpp", "cmake", "qmljs", -- C/Cpp/Qt
-    "matlab", -- Matlab
-    "java", -- Java
-    "python", -- Python
-    "r", -- R
-    "sql", -- SQL
+    "matlab",                     -- Matlab
+    "java",                       -- Java
+    "python",                     -- Python
+    "r",                          -- R
+    "sql",                        -- SQL
 
     -- 前端
     "html", "javascript", "typescript", "css", -- HTML/JS/TS/CSS
-    "vue", -- Vue
+    "vue",                                     -- Vue
   },
 
   highlight = {
     enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
     -- list of language that will be disabled
     -- disable = { "c", "rust" },
     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
@@ -40,9 +36,7 @@ require("nvim-treesitter.configs").setup {
       end
     end
   },
-  indent = {
-    enable = true
-  },
+  indent = { enable = true },
 
   -- 不同括号颜色区分（彩虹括号）
   rainbow = {
--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,16 +1,17 @@
-local bufferline = require('bufferline')
+local bufferline_ok, bufferline = pcall(require, "bufferline")
+if not bufferline_ok then
+  return
+end
 
 local icons = require("icons")
 
 bufferline.setup {
   options = {
-    close_command = "bp | bd #",
-    right_mouse_command = "bp | bd #",
     style_preset = bufferline.style_preset.no_italic,
     -- 使用 nvim 内置lsp
     diagnostics = "nvim_lsp",
-    -- 左侧让出 nvim-tree 的位置
     offsets = {
+      -- 左侧 neotree
       {
         filetype = "neo-tree",
         text = icons.basic.Vim .. " File Explorer",
@@ -18,32 +19,21 @@ bufferline.setup {
         text_align = "left",
         separator = true
       },
+      -- 右侧 Outline
       {
         filetype = "sagaoutline",
         text = icons.basic.Menu .. " Outline",
+        highlight = "Directory",
         text_align = "left",
         separator = true
       },
     },
     -- 分隔样式
-    indicator = {
-      icon = ' ', -- this should be omitted if indicator style is not 'icon'
-      style = 'icon',
-    },
-    -- hover 显示关闭按钮
-    -- hover = {
-    --   enabled = true,
-    --   delay = 200,
-    --   reveal = {'close'}
-    -- },
-    show_buffer_icons = true, -- disable filetype icons for buffers
-    show_buffer_close_icons = true,
+    indicator = { style = 'none' },
+    separator_style = { '│', '│' },
   },
 
   highlights = {
-    fill = {
-      fg = "#539BF5",
-      bg = "NONE"
-    }
+    separator = { fg = "#768390" },
   }
 }
