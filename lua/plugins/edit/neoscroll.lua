4da7b91ded1e1759c2c25cf84559fe9082e627eb
CWorld <cworld0@qq.com>
2023-08-04 09:12:19 +0800

    feat(plugins): :sparkles: plenties of plugins back

---
+++ lua/plugins/edit/neoscroll.lua
@@ -0,0 +1,15 @@
+-- 平滑滚动
+return {
+  'karb94/neoscroll.nvim',
+  lazy = true,
+  opts = {
+    mappings = {
+      '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>',
+      'zt', 'zz', 'zb'
+    },
+  },
+  keys = {
+    '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>',
+    'zt', 'zz', 'zb'
+  }
+}
---
+++ lua/plugins/edit/pairs.lua
@@ -0,0 +1,7 @@
+-- auto pairs 括号配对
+-- https://github.com/echasnovski/mini.pairs
+return {
+  "echasnovski/mini.pairs",
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {},
+}
