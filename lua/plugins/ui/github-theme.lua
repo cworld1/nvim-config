a86e95b8416b201ef399d48b2c1b7420e982a964
CWorld <cworld0@qq.com>
2023-08-04 09:11:31 +0800

    perf: :art: update ui look

--- lua/plugins/ui/github-theme.lua
+++ lua/plugins/ui/github-theme.lua
@@ -2,19 +2,13 @@
 -- https://github.com/projekt0n/github-nvim-theme
 return {
   'projekt0n/github-nvim-theme',
-  lazy = false,      -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000,   -- make sure to load this before all the other start plugins
+  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  -- build = ":GithubThemeCompile", -- lua api version
   config = function()
     require('github-theme').setup {
       options = {
-        styles = { comments = 'NONE' },
-        darken = {
-          sidebars = { enable = true }           -- 启用侧边栏
-        },
         modules = {
-          -- coc = {
-          --   background = true,
-          -- },
           diagnostic = {
             -- This is linked to so much that is needs to be enabled. This is here primarily
             -- for the extra options that can be added with modules
@@ -26,7 +20,12 @@ return {
             background = true,
           },
           treesitter = true,
-          lsp_semantic_tokens = true,
+          gitsigns = true,
+          neotree = true,
+          telescope = true,
+          dashboard = true,
+          indent_blankline = true,
+          whichkey = true,
         },
       }
     }
--- lua/plugins/ui/transparent.lua
+++ lua/plugins/ui/transparent.lua
@@ -2,22 +2,25 @@
 -- https://github.com/xiyaowong/transparent.nvim
 return {
   "xiyaowong/nvim-transparent",
+  lazy = false,
   config = function()
     require("transparent").setup({
       groups = { -- table: default groups
-                'Normal', 'NormalNC', 'Comment', 'Constant', 'Special',
-                'Identifier','Statement', 'PreProc', 'Type','Underlined',
-                'Todo', 'String', 'Function', 'Conditional', 'Repeat',
-        'Operator', 'Structure', 'LineNr', 'NonText','SignColumn',
-                'CursorLineNr', 'EndOfBuffer',
+        'Normal', 'NormalNC', 'Comment', 'Constant', 'Special',
+        'Identifier', 'Statement', 'PreProc', 'Type', 'Underlined',
+        'Todo', 'String', 'Function', 'Conditional', 'Repeat',
+        'Operator', 'Structure', 'LineNr', 'NonText', 'SignColumn',
+        'CursorLineNr', 'EndOfBuffer',
       },
-            extra_groups = {
-                'NeoTreeNormal', 'NeotreeNormalNC',
-                'NeoTreeTabInactive', 'NeoTreeTabSeparatorInactive', 'NeoTreeTabSeparatorActive',
-              }, -- table: additional groups that should be cleared
-      exclude_groups = {}                                                                   -- table: groups you don't want to clear
+      extra_groups = {
+        'NeoTreeNormal', 'NeotreeNormalNC',
+      },                  -- table: additional groups that should be cleared
+      exclude_groups = {} -- table: groups you don't want to clear
     })
 
     vim.cmd('TransparentEnable')
-  end
+  end,
+  keys = {
+    { "<leader>t", "<Cmd>TransparentToggle<CR>", desc = "Transparent Toggle" },
+  }
 }
