7d21c18639755b9e8ba70623d831757a06600891
CWorld <cworld0@qq.com>
2023-08-09 04:18:41 +0800

    feat(config): :sparkles: add vscode support

---
+++ lua/plugins/extras/vscode.lua
@@ -0,0 +1,37 @@
+if not vim.g.vscode then
+  return {}
+end
+
+local enabled = {
+  "lazy.nvim",
+  "Comment.nvim",
+  "mini.pairs",
+  "mini.surround",
+  "nvim-treesitter",
+  -- "nvim-treesitter-textobjects",
+}
+
+local Config = require("lazy.core.config")
+-- local Plugin = require("lazy.core.plugin")
+Config.options.checker.enabled = false
+Config.options.change_detection.enabled = false
+Config.options.defaults.cond = function(plugin)
+  return vim.tbl_contains(enabled, plugin.name) or plugin.vscode
+end
+
+-- Add some vscode specific keymaps
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyVimKeymaps",
+  callback = function()
+    vim.keymap.set("n", "<leader><space>", "<cmd>Find<cr>")
+    vim.keymap.set("n", "<leader>/", [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]])
+    vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]])
+  end,
+})
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { highlight = { enable = false } },
+  },
+}
