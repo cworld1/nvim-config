0ff275217cfb8663f1b3226a071faea80a679d0c
CWorld <cworld0@qq.com>
2023-09-06 21:34:39 +0800

    perf(vscode): :rocket: continue perf of vscode

--- lua/plugins/extras/vscode.lua
+++ lua/plugins/extras/vscode.lua
@@ -26,6 +26,7 @@ vim.api.nvim_create_autocmd("User", {
     vim.keymap.set("n", "<leader><space>", "<cmd>Find<cr>")
     vim.keymap.set("n", "<leader>/", [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]])
     vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]])
+    vim.keymap.set("n", "<leader>w", ":Write<CR>", { noremap = true })
   end,
 })
 
