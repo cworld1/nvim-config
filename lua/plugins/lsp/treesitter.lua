646d8e250089d42cbd7b4277cd7fafffbf8ed9da
CWorld <cworld0@qq.com>
2023-09-06 21:36:10 +0800

    perf(lsp): :rocket: update treesitter file

--- lua/plugins/lsp/treesitter.lua
+++ lua/plugins/lsp/treesitter.lua
@@ -7,32 +7,10 @@ return {
   event = { "BufReadPost", "BufNewFile" },
   dependencies = { "HiPhish/nvim-ts-rainbow2" },
   cmd = { "TSUpdateSync" },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        -- 静态文件
-        "vim", "vimdoc", "lua",        -- Neovim 自身
-        "markdown", "markdown_inline", -- Markdown
-        "json", "yaml", "toml",        -- 数据存储
-        "gitignore", "dockerfile",     -- 其他
-
-        -- 脚本文件
-        "bash",
-
-        -- 编程语言
-        "c", "cpp", "cmake", "qmljs", -- C/Cpp/Qt
-        "matlab",                     -- Matlab
-        "java",                       -- Java
-        "python",                     -- Python
-        "r",                          -- R
-        "sql",                        -- SQL
-
-        -- 前端
-        "html", "javascript", "typescript", "css", -- HTML/JS/TS/CSS
-        "vue",                                     -- Vue
-      },
-    })
-  end,
+  keys = {
+    { "<c-space>", desc = "Increment selection" },
+    { "<bs>", desc = "Decrement selection", mode = "x" },
+  },
   opts = {
     highlight = {
       enable = true,
@@ -42,9 +20,43 @@ return {
         if ok and stats and stats.size > max_filesize then
           return true
         end
-      end
+      end,
     },
     indent = { enable = true },
+    ensure_installed = {
+      -- 静态文件
+      "vim",
+      "vimdoc",
+      "lua", -- Neovim 自身
+      "markdown",
+      "markdown_inline", -- Markdown
+      "json",
+      "yaml",
+      "toml", -- 数据存储
+      "gitignore",
+      "dockerfile", -- 其他
+
+      -- 脚本文件
+      "bash",
+
+      -- 编程语言
+      "c",
+      "cpp",
+      "cmake",
+      "qmljs", -- C/Cpp/Qt
+      "matlab", -- Matlab
+      "java", -- Java
+      "python", -- Python
+      "r", -- R
+      "sql", -- SQL
+
+      -- 前端
+      "html",
+      "javascript",
+      "typescript",
+      "css", -- HTML/JS/TS/CSS
+      "vue", -- Vue
+    },
     incremental_selection = {
       enable = true,
       keymaps = {
@@ -60,19 +72,33 @@ return {
       -- list of languages you want to disable the plugin for
       -- disable = { 'jsx', 'cpp' },
       -- Which query to use for finding delimiters
-      query = 'rainbow-parens',
+      query = "rainbow-parens",
       -- Highlight the entire buffer all at once
       strategy = function()
-        return require('ts-rainbow').strategy.global
+        return require("ts-rainbow").strategy.global
       end,
       hlgroups = {
-        'TSRainbowYellow',
-        'TSRainbowBlue',
-        'TSRainbowOrange',
-        'TSRainbowGreen',
-        'TSRainbowViolet',
-        'TSRainbowCyan'
+        "TSRainbowYellow",
+        "TSRainbowBlue",
+        "TSRainbowOrange",
+        "TSRainbowGreen",
+        "TSRainbowViolet",
+        "TSRainbowCyan",
       },
-    }
+    },
   },
+  config = function(_, opts)
+    if type(opts.ensure_installed) == "table" then
+      ---@type table<string, boolean>
+      local added = {}
+      opts.ensure_installed = vim.tbl_filter(function(lang)
+        if added[lang] then
+          return false
+        end
+        added[lang] = true
+        return true
+      end, opts.ensure_installed)
+    end
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
