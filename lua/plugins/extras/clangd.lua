353fdc3d34ecec2bc331678a015f3eff38e69b30
CWorld <cworld0@qq.com>
2023-09-06 21:27:01 +0800

    feat(clangd): :sparkles: add windows support; fix and perf

--- lua/plugins/extras/clangd.lua
+++ lua/plugins/extras/clangd.lua
@@ -1,3 +1,19 @@
+local clangd_cmd = {
+  "clangd",
+  "--background-index",
+  "--clang-tidy",
+  "--header-insertion=iwyu",
+  "--completion-style=detailed",
+  "--function-arg-placeholders",
+  "--fallback-style=llvm",
+}
+if vim.g.is_windows then
+  table.insert(
+    clangd_cmd,
+    "--query-driver=D:\\Source\\mingw64\\bin\\c++.exe,D:\\Source\\mingw64\\bin\\gcc.exe"
+  )
+end
+
 return {
 
   -- Add C/C++ to treesitter
@@ -10,38 +26,38 @@ return {
     end,
   },
 
-  {
-    "p00f/clangd_extensions.nvim",
-    lazy = true,
-    config = function() end,
-    opts = {
-      extensions = {
-        inlay_hints = {
-          inline = false,
-        },
-        ast = {
-          --These require codicons (https://github.com/microsoft/vscode-codicons)
-          role_icons = {
-            type = "",
-            declaration = "",
-            expression = "",
-            specifier = "",
-            statement = "",
-            ["template argument"] = "",
-          },
-          kind_icons = {
-            Compound = "",
-            Recovery = "",
-            TranslationUnit = "",
-            PackExpansion = "",
-            TemplateTypeParm = "",
-            TemplateTemplateParm = "",
-            TemplateParamObject = "",
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "p00f/clangd_extensions.nvim",
+  --   lazy = true,
+  --   config = function() end,
+  --   opts = {
+  --     extensions = {
+  --       inlay_hints = {
+  --         inline = false,
+  --       },
+  --       ast = {
+  --         --These require codicons (https://github.com/microsoft/vscode-codicons)
+  --         role_icons = {
+  --           type = "",
+  --           declaration = "",
+  --           expression = "",
+  --           specifier = "",
+  --           statement = "",
+  --           ["template argument"] = "",
+  --         },
+  --         kind_icons = {
+  --           Compound = "",
+  --           Recovery = "",
+  --           TranslationUnit = "",
+  --           PackExpansion = "",
+  --           TemplateTypeParm = "",
+  --           TemplateTemplateParm = "",
+  --           TemplateParamObject = "",
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 
   -- Correctly setup lspconfig for clangd 🚀
   {
@@ -69,16 +85,7 @@ return {
           capabilities = {
             offsetEncoding = { "utf-16" },
           },
-          cmd = {
-            "clangd",
-            "--background-index",
-            "--clang-tidy",
-            "--header-insertion=iwyu",
-            "--completion-style=detailed",
-            "--function-arg-placeholders",
-            "--fallback-style=llvm",
-            "--query-driver=D:\\Source\\msys64\\ucrt64\\bin\\c++.exe,D:\\Source\\msys64\\ucrt64\\bin\\gcc.exe",
-          },
+          cmd = clangd_cmd,
           init_options = {
             usePlaceholders = true,
             completeUnimported = true,
@@ -87,71 +94,71 @@ return {
         },
       },
       setup = {
-        clangd = function(_, opts)
-          local clangd_ext_opts = require("util").opts("clangd_extensions.nvim")
-          require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
-          return true
-        end,
+        -- clangd = function(_, opts)
+        --   local clangd_ext_opts = require("util").opts("clangd_extensions.nvim")
+        --   require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+        --   return true
+        -- end,
       },
     },
   },
 
-  {
-    "nvim-cmp",
-    opts = function(_, opts)
-      table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
-    end,
-  },
+  -- {
+  --   "nvim-cmp",
+  --   opts = function(_, opts)
+  --     table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
+  --   end,
+  -- },
 
-  {
-    "mfussenegger/nvim-dap",
-    optional = true,
-    dependencies = {
-      -- Ensure C/C++ debugger is installed
-      "williamboman/mason.nvim",
-      optional = true,
-      opts = function(_, opts)
-        if type(opts.ensure_installed) == "table" then
-          vim.list_extend(opts.ensure_installed, { "codelldb" })
-        end
-      end,
-    },
-    opts = function()
-      local dap = require("dap")
-      if not dap.adapters["codelldb"] then
-        require("dap").adapters["codelldb"] = {
-          type = "server",
-          host = "localhost",
-          port = "${port}",
-          executable = {
-            command = "codelldb",
-            args = {
-              "--port",
-              "${port}",
-            },
-          },
-        }
-      end
-      for _, lang in ipairs({ "c", "cpp" }) do
-        dap.configurations[lang] = {
-          {
-            type = "codelldb",
-            request = "launch",
-            name = "Launch file",
-            program = function()
-              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-            end,
-            cwd = "${workspaceFolder}",
-          },
-          {
-            type = "codelldb",
-            request = "attach",
-            name = "Attach to process",
-            processId = require("dap.utils").pick_process,
-            cwd = "${workspaceFolder}",
-          },
-        }
-      end
-    end,
-  },
+  -- {
+  --   "mfussenegger/nvim-dap",
+  --   optional = true,
+  --   dependencies = {
+  --     -- Ensure C/C++ debugger is installed
+  --     "williamboman/mason.nvim",
+  --     optional = true,
+  --     opts = function(_, opts)
+  --       if type(opts.ensure_installed) == "table" then
+  --         vim.list_extend(opts.ensure_installed, { "codelldb" })
+  --       end
+  --     end,
+  --   },
+  --   opts = function()
+  --     local dap = require("dap")
+  --     if not dap.adapters["codelldb"] then
+  --       require("dap").adapters["codelldb"] = {
+  --         type = "server",
+  --         host = "localhost",
+  --         port = "${port}",
+  --         executable = {
+  --           command = "codelldb",
+  --           args = {
+  --             "--port",
+  --             "${port}",
+  --           },
+  --         },
+  --       }
+  --     end
+  --     for _, lang in ipairs({ "c", "cpp" }) do
+  --       dap.configurations[lang] = {
+  --         {
+  --           type = "codelldb",
+  --           request = "launch",
+  --           name = "Launch file",
+  --           program = function()
+  --             return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+  --           end,
+  --           cwd = "${workspaceFolder}",
+  --         },
+  --         {
+  --           type = "codelldb",
+  --           request = "attach",
+  --           name = "Attach to process",
+  --           processId = require("dap.utils").pick_process,
+  --           cwd = "${workspaceFolder}",
+  --         },
+  --       }
+  --     end
+  --   end,
+  -- },
 }
