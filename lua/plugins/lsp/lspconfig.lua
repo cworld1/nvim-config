6d021338e968dab9879611fa1ad94f933caa17ca
CWorld <cworld0@qq.com>
2023-10-07 18:17:03 +0800

    style(plugins/lsp): :zap: format lspconfig

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -113,7 +113,7 @@ return {
 
     if opts.inlay_hints.enabled and inlay_hint then
       Util.on_attach(function(client, buffer)
-        if client.supports_method('textDocument/inlayHint') then
+        if client.supports_method("textDocument/inlayHint") then
           inlay_hint(buffer, true)
         end
       end)
@@ -121,14 +121,14 @@ return {
 
     if type(opts.diagnostics.virtual_text) == "table" and opts.diagnostics.virtual_text.prefix == "icons" then
       opts.diagnostics.virtual_text.prefix = vim.fn.has("nvim-0.10.0") == 0 and "●"
-          or function(diagnostic)
-            local icons = require("icons").diagnostics
-            for d, icon in pairs(icons) do
-              if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
-                return icon
-              end
+        or function(diagnostic)
+          local icons = require("icons").diagnostics
+          for d, icon in pairs(icons) do
+            if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
+              return icon
             end
           end
+        end
     end
 
     vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
