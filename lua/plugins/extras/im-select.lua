050e1475c969b6810fa947c85eeacce1af10e695
CWorld <cworld0@qq.com>
2024-03-31 21:45:36 +0800

    perf(extras): :rocket: update im-select conf

--- lua/plugins/extras/im-select.lua
+++ lua/plugins/extras/im-select.lua
@@ -3,7 +3,7 @@
 return {
   {
     "keaising/im-select.nvim",
-    event = { "BufReadPost", "BufNewFile" },
+    event = { "InsertEnter" },
     opts = {
       -- IM will be set to `default_im_select` in `normal` mode
       -- For Windows/WSL, default: "1033", aka: English US Keyboard
@@ -13,7 +13,7 @@ return {
       --               "1" for Fcitx
       --               "xkb:us::eng" for ibus
       -- You can use `im-select` or `fcitx5-remote -n` to get the IM's name
-      default_im_select = "com.apple.keylayout.ABC",
+      default_im_select = "com.apple.keylayout.US",
 
       -- Can be binary's name or binary's full path,
       -- e.g. 'im-select' or '/usr/local/bin/im-select'
@@ -21,6 +21,21 @@ return {
       -- For macOS, default: "im-select"
       -- For Linux, default: "fcitx5-remote" or "fcitx-remote" or "ibus"
       -- default_command = "im-select.exe",
+
+      -- Restore the default input method state when the following events are triggered
+      set_default_events = { "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
+
+      -- Restore the previous used input method state when the following events
+      -- are triggered, if you don't want to restore previous used im in Insert mode,
+      -- e.g. deprecated `disable_auto_restore = 1`, just let it empty
+      -- as `set_previous_events = {}`
+      set_previous_events = { "InsertEnter" },
+
+      -- Show notification about how to install executable binary when binary missed
+      keep_quiet_on_no_binary = false,
+
+      -- Async run `default_command` to switch IM or not
+      async_switch_im = false,
     },
   },
 }
