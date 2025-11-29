88b664c476b8ed2cf057b94f01c68da48d6d8b1e
CWorld <cworld0@qq.com>
2025-11-29 09:37:37 +0800

    feat: :sparkles: more config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,101 @@
-local map = vim.keymap.set
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
-map("n", "<leader>b", "<cmd>BufferLineCyclePrev<cr><cmd>bd #<cr>", { desc = "Close buffer" }) -- close
-map("n", "<leader>cp", ":set spell!<CR>", { desc = "Toggle spelling" })
+-- [Basic]
+-- Quit
+vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
+-- Save
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+-- [View]
+vim.keymap.set("n", "<leader>cp", ":set spell!<CR>", { desc = "Toggle spelling" })
+vim.keymap.set('n', '<leader>cx', ':set wrap!<CR>', { desc = "Toggle wrap" })
+
+-- [Edit]
+-- Indent
+vim.keymap.set("x", "<", "<gv")
+vim.keymap.set("x", ">", ">gv")
+-- Comment
+vim.keymap.set("n", "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Below" })
+vim.keymap.set("n", "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Above" })
+-- Move lines
+vim.keymap.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+vim.keymap.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+vim.keymap.set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+vim.keymap.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+-- Manage buffer
+vim.keymap.set("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+vim.keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<leader>bd", function()
+  local cur = vim.api.nvim_get_current_buf()
+  local alt = vim.fn.bufnr("#")
+  if alt > 0 and vim.api.nvim_buf_is_loaded(alt) then
+    vim.cmd("buffer #")
+  else
+    vim.cmd("bnext")
+  end
+  vim.cmd("bdelete " .. cur)
+end, { desc = "Delete Buffer" })
+vim.keymap.set("n", "<leader>bo", function()
+  local current = vim.api.nvim_get_current_buf()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(buf) and buf ~= current then
+      vim.cmd("bdelete " .. buf)
+    end
+  end
+end, { desc = "Delete Other Buffers" })
+
+-- [Window]
+-- Split windows
+vim.keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
+vim.keymap.set("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
+-- Move between windows
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to below window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to above window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+-- Resize splits
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- [Functions]
+vim.keymap.set("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" }) -- new file
+-- File explorer
+vim.keymap.set("n", "<leader>e", ":Lexplore<CR>", { desc = "Toggle file explorer" })
+-- Terminal
+vim.keymap.set("n", "<leader>`", "<cmd>vert term fish.exe<CR>", { desc = "Open Term" })
+
+-- Search
+-- Better n/N behavior https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+vim.keymap.set("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+vim.keymap.set("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+vim.keymap.set("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+vim.keymap.set("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+vim.keymap.set("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+vim.keymap.set("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+-- Clear search and stop snippet on escape
+vim.keymap.set({ "i", "n", "s" }, "<esc>", function()
+  vim.cmd("noh")
+  return "<esc>"
+end, { expr = true, desc = "Escape and Clear hlsearch" })
+
+-- [Others]
+-- -- location list
+-- vim.keymap.set("n", "<leader>xl", function()
+--   local success, err = pcall(vim.fn.getloclist(0, { winid = 0 }).winid ~= 0 and vim.cmd.lclose or vim.cmd.lopen)
+--   if not success and err then
+--     vim.notify(err, vim.log.levels.ERROR)
+--   end
+-- end, { desc = "Location List" })
+-- -- quickfix list
+-- vim.keymap.set("n", "<leader>xq", function()
+--   local success, err = pcall(vim.fn.getqflist({ winid = 0 }).winid ~= 0 and vim.cmd.cclose or vim.cmd.copen)
+--   if not success and err then
+--     vim.notify(err, vim.log.levels.ERROR)
+--   end
+-- end, { desc = "Quickfix List" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,13 +1,63 @@
 -- [Appearance]
+vim.opt.termguicolors = true -- enable 24-bit RGB colors
+vim.opt.signcolumn = "yes"   -- always show sign column
+-- vim.opt.showmode = false -- Hide mode status
+-- Line edit
+vim.opt.cursorline = true  -- highlight current line
+vim.opt.scrolloff = 4      -- keep 4 lines visible around cursor
+vim.opt.sidescrolloff = 8  -- keep 8 columns visible horizontally
+-- Indent
+vim.opt.expandtab = true   -- use spaces instead of tabs
+vim.opt.shiftwidth = 2     -- indent size
+vim.opt.tabstop = 2        -- tab character width
+vim.opt.shiftround = true  -- round indent to nearest multiple of shiftwidth
+vim.opt.smartindent = true -- auto-indent new lines intelligently
+-- Line number
 vim.opt.number = true
-vim.opt.relativenumber = true
+-- Wrap
+vim.opt.wrap = false -- default not line wrap
+vim.opt.linebreak = true -- wrap at word boundary if wrap
+vim.opt.breakindent = true -- maintain indent on wrap
+-- Others
+vim.opt.winminwidth = 5 -- prevent tiny splits
+vim.opt.foldtext = "v:lua.vim.fn.getline(v:foldstart) .. ' …'" -- Fold text
 
 -- [Editor]
 vim.opt.fileformat = "unix"
+vim.opt.mouse = "a"                    -- enable mouse in all modes
+vim.opt.laststatus = 3                 -- Global statusline (once you add one)
+vim.opt.colorcolumn = "80"             -- Column ruler
+-- Case
+vim.opt.ignorecase = true              -- case-insensitive by default
+vim.opt.smartcase = true               -- but smart if uppercase is used
+-- Split
+vim.opt.splitbelow = true              -- horizontal splits below
+vim.opt.splitright = true              -- vertical splits to the right
+vim.opt.splitkeep = "screen"           -- preserve layout when splitting
+-- Format
+vim.opt.formatoptions = "jcroqlnt"     -- keep comments, wrap text, autoformat when possible
+-- Command
+vim.opt.inccommand = "nosplit"         -- live preview for :substitute
+vim.opt.wildmode = "longest:full,full" -- enhanced command completion
+-- Others
+vim.opt.jumpoptions = "view"           -- restore view after jump
+vim.opt.virtualedit = "block"          -- allow cursor past EOL in block mode
 
 -- [Functions]
 -- Clipboard
--- vim.opt.clipboard:append("unnamedplus")
+vim.opt.clipboard = vim.env.SSH_CONNECTION and "" or "unnamedplus"
 -- Check spelling
 vim.opt.spell = true
 vim.opt.spelllang = "en_us"
+-- File tree
+-- vim.cmd("let g:netrw_banner=0") -- disable banner
+vim.cmd("let g:netrw_liststyle=3") -- tree view list style
+
+-- [Auto commands]
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("lazyvim_highlight_yank", { clear = true }),
+  callback = function()
+    (vim.hl or vim.highlight).on_yank()
+  end,
+})
