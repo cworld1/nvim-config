2d182e7b8e2965908675f7614c0f5e3abfc43aa2
CWorld <cworld0@qq.com>
2025-12-02 02:47:52 +0800

    fix: :bug: fix pairs problem

--- lua/custom/pairs.lua
+++ lua/custom/pairs.lua
@@ -1,15 +1,54 @@
-local pair_map = { ['('] = ')', ['['] = ']', ['{'] = '}', ['"'] = '"', ["'"] = "'", ['`'] = '`' }
+local M = {}
 
-for l, r in pairs(pair_map) do
-  vim.keymap.set('i', l, l .. r .. '<Left>', { noremap = true })
-  vim.keymap.set('i', r, 'v:lua.MaybeSkip("' .. r .. '")', { expr = true, noremap = true })
+M.pairs = {
+  ['('] = ')',
+  ['['] = ']',
+  ['{'] = '}',
+  ['"'] = '"',
+  ["'"] = "'",
+  ['`'] = '`',
+}
+
+local function get_char(line, col)
+  return line:sub(col, col)
 end
 
-_G.MaybeSkip = function(ch)
-  local col = vim.fn.col('.') - 1
-  local line = vim.fn.getline('.')
-  if line:sub(col + 1, col + 1) == ch then
+function M.open(char)
+  return char .. M.pairs[char] .. '<Left>'
+end
+
+function M.close(open)
+  local close = M.pairs[open]
+  local line = vim.api.nvim_get_current_line()
+  local col = vim.fn.col('.')
+  if get_char(line, col) == close then
     return '<Right>'
   end
-  return ch
+  return close
 end
+
+function M.closeopen(char)
+  local close = M.pairs[char]
+  local line = vim.api.nvim_get_current_line()
+  local col = vim.fn.col('.')
+  if get_char(line, col) == close then
+    return '<Right>'
+  end
+  return M.open(char)
+end
+
+M.setup = function()
+  local opts = { expr = true, noremap = true, replace_keycodes = true }
+
+  -- Asymmetric
+  for open, close in pairs({ ['('] = ')', ['['] = ']', ['{'] = '}' }) do
+    vim.keymap.set('i', open, function() return M.open(open) end, opts)
+    vim.keymap.set('i', close, function() return M.close(open) end, opts)
+  end
+  -- Symmetric
+  for _, sym in ipairs({ '"', "'", '`' }) do
+    vim.keymap.set('i', sym, function() return M.closeopen(sym) end, opts)
+  end
+end
+
+return M
