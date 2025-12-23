0f92895d8956325b0e71b0a4c37b7c5d6fabc23e
CWorld <cworld0@qq.com>
2025-12-24 05:31:50 +0800

    feat(lsp): improve lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,14 @@ vim.pack.add({
   { src = "https://github.com/neovim/nvim-lspconfig" },
 })
 require("mason").setup({
+  ensure_installed = {
+    -- LSP
+    "vtsls",
+    -- Formatter
+    "stylua",
+    "prettier",
+    "shfmt",
+  },
   ui = {
     icons = {
       package_installed = "✓",
@@ -22,17 +30,41 @@ vim.api.nvim_create_autocmd("LspAttach", {
       return { buffer = ev.buf, desc = desc }
     end
     -- LSP keymaps
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("LSP Hover"))
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Goto Definition"))
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Goto Declaration"))
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts("List References"))
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Goto Implementation"))
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts("Type Definition"))
-    vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts("Rename Symbol"))
-    vim.keymap.set({ "n", "x" }, "<leader>ca", vim.lsp.buf.code_action, opts("Code Action"))
-    vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts("Signature Help"))
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("LSP hover"))
+    vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, opts("LSP hover"))
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Goto definition"))
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Goto declaration"))
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts("List references"))
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Goto implementation"))
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts("Type definition"))
+    vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts("Rename symbol"))
+    vim.keymap.set({ "n", "x" }, "<leader>ca", vim.lsp.buf.code_action, opts("Code action"))
+    vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts("Signature help"))
   end,
 })
+
+-- [Formatter]
+vim.pack.add({ "https://github.com/stevearc/conform.nvim" })
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
+})
+vim.keymap.set("n", "<leader>cf",
+  function()
+    require("conform").format({
+      async = true,
+      lsp_fallback = true,
+      timeout_ms = 500,
+    })
+  end,
+  { desc = "Format file" }
+)
 
 -- [Diagnostic]
 vim.pack.add({ "https://github.com/rachartier/tiny-inline-diagnostic.nvim" })
@@ -72,23 +104,3 @@ require("blink.cmp").setup({
   -- fuzzy = { implementation = "prefer_rust_with_warning" },
   fuzzy = { implementation = "lua" },
 })
-
--- [Formatter]
-vim.pack.add({ "https://github.com/stevearc/conform.nvim" })
-require("conform").setup({
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_format = "fallback",
-  },
-})
-vim.keymap.set("n", "<leader>cf",
-  function()
-    require("conform").format({
-      async = true,
-      lsp_fallback = true,
-      timeout_ms = 500,
-    })
-  end,
-  { desc = "Format file" }
-)
