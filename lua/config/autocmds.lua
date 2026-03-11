10af2ad26a83fedc2e81d5834cfc1bec221b6079
CWorld <cworld0@qq.com>
2026-03-11 17:27:48 +0800

    refactor(autocmds): add buftype check

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -37,10 +37,12 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   group = vim.api.nvim_create_augroup('WriteWithLF', {}),
   pattern = '*',
   callback = function()
-    if vim.bo.readonly or vim.bo.buftype ~= '' then return end
+    if vim.bo.readonly or vim.bo.buftype ~= '' or vim.bo.binary then return end
     vim.bo.fileformat = 'unix'
-    vim.cmd([[ %s/\r\+$//e ]])
-  end,
+    local view = vim.fn.winsaveview()
+    vim.cmd([[keeppatterns silent! %s/\r\+$//e]])
+    vim.fn.winrestview(view)
+  end
 })
 
 -- [Autocmd] Auto set root
@@ -94,6 +96,7 @@ vim.api.nvim_create_autocmd('BufEnter', {
   nested = true,
   callback = vim.schedule_wrap(function(data)
     if data.buf ~= vim.api.nvim_get_current_buf() then return end
+    if vim.bo[data.buf].buftype ~= '' then return end
     local root = find_root(data.buf, H.names, H.fallback)
     if root == nil then return end
     vim.fn.chdir(root)
