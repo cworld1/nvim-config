57dee21690697b54332a6e9f9e54622bac81f8f8
CWorld <cworld0@qq.com>
2026-03-11 17:38:14 +0800

    perf(plugins): lazy load im-select

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -1,36 +1,42 @@
 -- Auto switch input method
+local lazy = require('libs.lazy')
 
-vim.pack.add({ 'https://github.com/keaising/im-select.nvim' })
-require('im_select').setup({
-  -- IM will be set to `default_im_select` in `normal` mode
-  -- For Windows/WSL, default: "1033", aka: English US Keyboard
-  -- For macOS, default: "com.apple.keylayout.ABC", aka: US
-  -- For Linux, default:
-  --               "keyboard-us" for Fcitx5
-  --               "1" for Fcitx
-  --               "xkb:us::eng" for ibus
-  -- You can use `im-select` or `fcitx5-remote -n` to get the IM's name
-  -- default_im_select = '0',
+lazy.load({
+  plugin = 'https://github.com/keaising/im-select.nvim',
+  event = { 'InsertEnter' },
+  setup = function()
+    require('im_select').setup({
+      -- IM will be set to `default_im_select` in `normal` mode
+      -- For Windows/WSL, default: "1033", aka: English US Keyboard
+      -- For macOS, default: "com.apple.keylayout.ABC", aka: US
+      -- For Linux, default:
+      --               "keyboard-us" for Fcitx5
+      --               "1" for Fcitx
+      --               "xkb:us::eng" for ibus
+      -- You can use `im-select` or `fcitx5-remote -n` to get the IM's name
+      -- default_im_select = '0',
 
-  -- Can be binary's name, binary's full path, or a table, e.g. 'im-select',
-  -- '/usr/local/bin/im-select', or { "AIMSwitcher.exe", "--imm" }
-  -- For Windows/WSL, default: "im-select.exe"
-  -- For macOS, default: "im-select"
-  -- For Linux, default: "fcitx5-remote" or "fcitx-remote" or "ibus"
-  -- default_command = { 'AIMSwitcher.exe', '--imm' },
+      -- Can be binary's name, binary's full path, or a table, e.g. 'im-select',
+      -- '/usr/local/bin/im-select', or { "AIMSwitcher.exe", "--imm" }
+      -- For Windows/WSL, default: "im-select.exe"
+      -- For macOS, default: "im-select"
+      -- For Linux, default: "fcitx5-remote" or "fcitx-remote" or "ibus"
+      -- default_command = { 'AIMSwitcher.exe', '--imm' },
 
-  -- Restore the default input method state when the following events are triggered
-  set_default_events = { 'VimEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave' },
+      -- Restore the default input method state when the following events are triggered
+      set_default_events = { 'VimEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave' },
 
-  -- Restore the previous used input method state when the following events
-  -- are triggered, if you don't want to restore previous used im in Insert mode,
-  -- e.g. deprecated `disable_auto_restore = 1`, just let it empty
-  -- as `set_previous_events = {}`
-  set_previous_events = { 'InsertEnter' },
+      -- Restore the previous used input method state when the following events
+      -- are triggered, if you don't want to restore previous used im in Insert mode,
+      -- e.g. deprecated `disable_auto_restore = 1`, just let it empty
+      -- as `set_previous_events = {}`
+      set_previous_events = { 'InsertEnter' },
 
-  -- Show notification about how to install executable binary when binary missed
-  keep_quiet_on_no_binary = false,
+      -- Show notification about how to install executable binary when binary missed
+      keep_quiet_on_no_binary = false,
 
-  -- Async run `default_command` to switch IM or not
-  async_switch_im = false,
+      -- Async run `default_command` to switch IM or not
+      async_switch_im = false,
+    })
+  end
 })
