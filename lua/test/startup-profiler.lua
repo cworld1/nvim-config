bc7e02c69302bfe4cdaa2d9d29fd91c5369b43a4
CWorld <cworld0@qq.com>
2026-03-25 23:10:54 +0800

    fix(test): fix startup-profiler test

--- lua/test/startup-profiler.lua
+++ lua/test/startup-profiler.lua
@@ -3,15 +3,22 @@ local utils = require('libs.utils')
 --[Startup Profiler]
 -- Only start when `PROF=1 nvim`
 if vim.env.PROF then
+  if not utils.is_compatible_version('0.12') then
+    vim.notify('Need Neovim 0.12+', vim.log.levels.ERROR)
+    return
+  end
+
   vim.cmd('packadd snacks.nvim')
-  require('snacks.profiler').startup({
+  local profiler = require('snacks.profiler')
+  profiler.startup({
     startup = {
       event = 'VimEnter', -- Stop record after startup finished
     },
   })
-end
 
-if utils.is_compatible_version('0.12') then
-  vim.notify('Need Neovim 0.12+', vim.log.levels.ERROR)
-  return
+  -- Keymap
+  vim.keymap.set('n', '<leader>pP', function() profiler.toggle() end,
+    { desc = 'Toggle profiler' })
+  vim.keymap.set('n', '<leader>ps', function() profiler.scratch() end,
+    { desc = 'Profiler scratch buffer' })
 end
