1416bc3a7aa9e9000180e6045b476c8598d37f60
CWorld <cworld0@qq.com>
2026-03-25 15:16:22 +0800

    feat(tool): add new plugin flash.nvim

--- lua/plugins/tool.lua
+++ lua/plugins/tool.lua
@@ -58,6 +58,21 @@ lazy.load({
   end
 })
 
+-- [Word jump]
+lazy.load({
+  plugin = 'https://github.com/folke/flash.nvim',
+  event = { 'BufReadPost', 'BufNewFile' },
+  keys = {
+    { { 'n', 'x', 'o' }, 's', function() require('flash').jump() end, { desc = 'Flash' } },
+    { { 'n', 'x', 'o' }, 'S', function() require('flash').treesitter() end, { desc = 'Flash treesitter' } },
+    { 'o', 'r', function() require('flash').remote() end, { desc = 'Remote flash' } },
+    { { 'o', 'x' }, 'R', function() require('flash').treesitter_search() end, { desc = 'Treesitter search' } },
+    { { 'c' }, '<c-s>', function() require('flash').toggle() end, { desc = 'Toggle flash search' } }
+  },
+  setup = function() require('flash').setup() end
+})
+
+
 -- [Clipboard]
 -- vim.pack.add({ "https://github.com/gbprod/yanky.nvim" })
 -- -- Custom paste function
