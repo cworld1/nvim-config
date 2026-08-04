c1a7f38005c637b09ba27eca67a4b8d57222aa77
CWorld <cworld0@qq.com>
2026-08-05 00:39:29 +0800

    perf(tool): remove flash.nvim

--- lua/plugins/tool.lua
+++ lua/plugins/tool.lua
@@ -59,18 +59,18 @@ lazy.load({
 })
 
 -- [Word jump]
-lazy.load({
-  plugin = 'https://github.com/folke/flash.nvim',
-  event = { 'BufReadPost', 'BufNewFile' },
-  keys = {
-    { { 'n', 'x', 'o' }, 's', function() require('flash').jump() end, { desc = 'Flash' } },
-    { { 'n', 'x', 'o' }, 'S', function() require('flash').treesitter() end, { desc = 'Flash treesitter' } },
-    { 'o', 'r', function() require('flash').remote() end, { desc = 'Remote flash' } },
-    { { 'o', 'x' }, 'R', function() require('flash').treesitter_search() end, { desc = 'Treesitter search' } },
-    { { 'c' }, '<c-s>', function() require('flash').toggle() end, { desc = 'Toggle flash search' } }
-  },
-  setup = function() require('flash').setup() end
-})
+-- lazy.load({
+--   plugin = 'https://github.com/folke/flash.nvim',
+--   event = { 'BufReadPost', 'BufNewFile' },
+--   keys = {
+--     { { 'n', 'x', 'o' }, 's', function() require('flash').jump() end, { desc = 'Flash' } },
+--     { { 'n', 'x', 'o' }, 'S', function() require('flash').treesitter() end, { desc = 'Flash treesitter' } },
+--     { 'o', 'r', function() require('flash').remote() end, { desc = 'Remote flash' } },
+--     { { 'o', 'x' }, 'R', function() require('flash').treesitter_search() end, { desc = 'Treesitter search' } },
+--     { { 'c' }, '<c-s>', function() require('flash').toggle() end, { desc = 'Toggle flash search' } }
+--   },
+--   setup = function() require('flash').setup() end
+-- })
 
 
 -- [Clipboard]
