c55a57597694aebb89c609f376a6c9f51a762090
CWorld <cworld0@qq.com>
2026-09-06 17:35:26 +0800

    perf(custom): improve yazi

--- lua/custom/yazi.lua
+++ lua/custom/yazi.lua
@@ -1,51 +1,54 @@
 local M = {}
 
+local function open_selected_files(tmpfile)
+  local file = io.open(tmpfile, 'r')
+  if not file then
+    return
+  end
+  local first = true
+  for path in file:lines() do
+    if path ~= '' then
+      vim.cmd({
+        cmd = first and 'edit' or 'badd',
+        args = { path },
+      })
+
+      first = false
+    end
+  end
+  file:close()
+end
+
 function M.open()
   if vim.fn.executable('yazi') == 0 then
     vim.notify('Yazi is not installed or not in PATH!', vim.log.levels.ERROR)
     return
   end
 
-  local tmpfile = os.tmpname()
+  local tmpfile = vim.fn.tempname()
   local win = require('snacks').win({
     title = 'Yazi',
     border = 'single',
     backdrop = true,
     minimal = true,
   })
-
-  vim.fn.jobstart(string.format('yazi --chooser-file="%s"', tmpfile), {
+  vim.fn.jobstart({ 'yazi', '--chooser-file', tmpfile }, {
     term = true,
-    on_exit = function(_, code, _)
+    on_exit = function(_, code)
       vim.schedule(function()
         win:close()
         if code == 0 then
-          local fd = io.open(tmpfile, 'r')
-          if fd then
-            local is_first = true
-            for target_file in fd:lines() do
-              if target_file and target_file ~= '' then
-                if is_first then
-                  vim.cmd.edit(target_file)
-                  is_first = false
-                else
-                  vim.cmd.badd(target_file)
-                end
-              end
-            end
-            fd:close()
-            os.remove(tmpfile)
-          end
+          open_selected_files(tmpfile)
         end
+        os.remove(tmpfile)
       end)
-    end
+    end,
   })
-
   vim.cmd('startinsert')
 end
 
 function M.setup()
-  vim.keymap.set('n', '<leader>fy', function() M.open() end, { desc = 'Find via Yazi' })
+  vim.keymap.set('n', '<leader>fy', M.open, { desc = 'Find via Yazi', })
 end
 
 return M
